96ac86b5ad1ac400d2c99d8dafb0a3491e5d12ae
aidan.dev <dev.aidan@lcincom.com>
2021-09-21 01:16:57 +0700

    refactor code

--- api/competition/controllers/competition.js
+++ api/competition/controllers/competition.js
@@ -6,25 +6,37 @@ module.exports = {
     const file = path.join(__dirname, "../../data/competitions.json");
     let rawdata = fs.readFileSync(file);
     let data = JSON.parse(rawdata);
-
-    const competitions = await Promise.all(
+    const { plan } = ctx.query;
+    const res = await axios.get(
+      `${process.env.FB_URL}/competitions?plan=${plan}`,
+      {
+        headers: fbHeader,
+      }
+    );
+    const { competitions } = res.data;
+    await Promise.all(
       data.map(async ({ id, name, code, plan, img, currentSeason, arid }) => {
         const area = await strapi.services.area.findOne({ aid: arid });
-        return await strapi.services.competition.create({
+        const competition = await strapi.services.competition.findOne({
           cid: id,
-          name_en: name,
-          code_en: code,
-          plan,
-          img,
-          area,
         });
+        if (!competition) {
+          await strapi.services.competition.create({
+            cid: id,
+            name_en: name,
+            code_en: code,
+            plan,
+            img,
+            area,
+          });
+        }
       })
     );
 
-    console.log(competitions);
     ctx.send({
       message: "sync competition success",
       status: 200,
+      data: competitions,
     });
   },
 };
